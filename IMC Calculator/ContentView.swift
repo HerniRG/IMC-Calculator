8aebde6677143582b7478c919aff949433695d57
HernánRG <hernanrg85@gmail.com>
2024-07-30 16:58:28 +0200

    Initial Commit

---
+++ IMC Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IMC Calculator
+//
+//  Created by Hernán Rodríguez on 30/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IMC Calculator/IMC_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMC_CalculatorApp.swift
+//  IMC Calculator
+//
+//  Created by Hernán Rodríguez on 30/7/24.
+//
+
+import SwiftUI
+
+@main
+struct IMC_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
