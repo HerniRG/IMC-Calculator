439072d651ec1272f14c8089aedc1fd30111ec9a
hernirg <hernanrg85@gmail.com>
2024-07-31 18:58:49 +0200

    pantalla resultado terminado

---
+++ IMC Calculator/Components/InformationViewResult.swift
@@ -0,0 +1,18 @@
+//
+//  InformationViewResult.swift
+//  IMC Calculator
+//
+//  Created by Hernán Rodríguez on 31/7/24.
+//
+
+import SwiftUI
+
+struct InformationViewResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InformationViewResult()
+}
